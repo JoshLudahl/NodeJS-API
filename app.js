c9c6fd3708a153ee0078045711358aa791d633cd
Josh Ludahl <josh.ludahl@gmail.com>
2019-07-15 19:17:05 -0700

    updated some snacks

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -10,35 +9,33 @@ const jwt = require('jsonwebtoken');
 app.set('view engine', 'ejs');
 
 //  connect to database
-require('./db/db')
-
-
-
+require('./db/db_config');
 
 //  ENABLE LOGGING
 app.use(morgan('dev'));
 //  ENABLE BODY PARSING
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({}));
 
 app.use(express.static('uploads'));
 //  ENABLE CORS ACCESS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
-
 
 //  ROUTES
 //      PRODUCT ROUTE
 const productRoutes = require('./api/routes/products');
 app.use('/products', productRoutes);
-
 
 //      ORDERS ROUTE
 const ordersRoutes = require('./api/routes/orders');
@@ -53,18 +50,18 @@ app.use('/user', userRoutes);
 
 //  CUSTOM ERROR HANDLING
 app.use((req, res, next) => {
-    const error = new Error('404 Not found');
-    error.status = 404;
-    next(error);
+  const error = new Error('404 Not found');
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
